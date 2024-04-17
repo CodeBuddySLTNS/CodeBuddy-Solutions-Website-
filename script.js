65f07bce14b49d381f7f19e8d81fe4b712dc182b
CodeBuddySLTNS <renz2arjhay@gmail.com>
2024-04-18 04:28:14 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuIcon = document.querySelector('.menuIcon');
+const navLinks = document.querySelector('.navLinks');
+
+menuIcon.onclick = () => {
+  if(navLinks.offsetHeight == 0){
+    navLinks.style.height = "269px";
+    console.log(navLinks.offsetHeight)
+    menuIcon.classList.add("fa-xmark");
+  }else{
+    navLinks.style.height = "0";
+    menuIcon.classList.remove("fa-xmark");
+  }
+}
